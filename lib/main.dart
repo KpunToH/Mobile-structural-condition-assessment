66cabeb840d538c6b7e8cbf384da4fcccde85a5c
Pinem3 <n.m.post381@gmail.com>
2023-03-02 18:42:40 +0300

    Добавлены пустые кнопки

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,47 @@
 import 'package:flutter/material.dart';
+//import 'package:image_picker/image_picker.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    home: UserPannel(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class UserPannel extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    	return MaterialApp(
-      	title: 'Flutter Demo',
-      	theme: ThemeData(
-        		primarySwatch: Colors.green,
-      		),
-      	home: Scaffold(
-			appBar: AppBar(title: const Text('Moscas'),),
-			body: const Text ('Welcome!', style: TextStyle(
-				fontSize: 30,
-				color: Color.fromARGB(255, 30, 30, 40),
-			),)
-			)
-    		);
-  	}
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 63, 60, 44),
+      body: SafeArea(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(padding: EdgeInsets.only(top: 30.0),),
+                FilledButton(
+                  onPressed: () {},
+                  child: Text('Введите имя проекта', 
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 206, 190, 144)
+                    )),
+                  style: ButtonStyle(backgroundColor: MaterialStatePropertyAll( Color.fromARGB(255, 53, 50, 40))),
+                ),
+                FilledButton(
+                  onPressed: () {
+                  },
+                  child: Text('Выберите файл', 
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 206, 190, 144)
+                    )),
+                  style: ButtonStyle(backgroundColor: MaterialStatePropertyAll( Color.fromARGB(255, 53, 50, 40))),
+                )
+              ],
+            )
+          ]
+        )
+      )
+    );
+  }
 }
